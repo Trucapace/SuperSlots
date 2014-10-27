0f4d38d88b46790eafce8e55515121c721704510
David Blanck <dblanck@wowway.com>
2014-10-27 13:45:52 -0400

    first commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by David Blanck on 10/26/14.
+//  Copyright (c) 2014 VidWare. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
